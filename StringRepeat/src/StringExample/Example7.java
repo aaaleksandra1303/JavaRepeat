e8eee7e989f3ebac40d6c45a50cfd65d5ae0c630
aaaleksandra1303 <aleksandra1303@gmail.com>
2017-05-20 18:51:21 +0200

    Print sentence with Quotation marks

---
+++ StringRepeat/src/StringExample/Example7.java
@@ -0,0 +1,19 @@
+package StringExample;
+
+public class Example7 {
+
+	/*
+	 * Zadanie7. Wypisz na ekran następujący komunikat (uwzględniając cytat):
+“Mowa jest srebrem, a milczenie złotem”
+	 */
+	public static void main(String[] args) {
+		
+		String sentence = "\"Mowa jest srebrem, a milczenie zlotem\"";
+		System.out.println(sentence);
+		
+		
+	
+
+	}
+
+}
