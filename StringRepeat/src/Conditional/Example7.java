8beab66e2e4aab7f1139a26591d97341d2077189
aaaleksandra1303 <aleksandra1303@gmail.com>
2017-05-22 12:02:35 +0200

    printing numbers from 1 to 100

---
+++ StringRepeat/src/Conditional/Example7.java
@@ -0,0 +1,20 @@
+package Conditional;
+
+public class Example7 {
+	/*
+	 * Zadanie7.
+Napisz program, który wypisuje na ekran wszystkie liczby od 1 do 100.
+	 */
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=100;i++)
+		{
+			System.out.println(i);
+		}
+	
+		
+
+	}
+
+}
