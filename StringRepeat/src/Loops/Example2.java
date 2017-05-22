05bb53d36805a0aa8a0a9c4e77253c933d0c0b98
aaaleksandra1303 <aleksandra1303@gmail.com>
2017-05-22 18:54:58 +0200

    Print all number from 10 to 100 with loop for

---
+++ StringRepeat/src/Loops/Example2.java
@@ -0,0 +1,17 @@
+package Loops;
+
+public class Example2 {
+	/*
+	 * Zadanie2. Napisz program, który wypisuje wszystkie liczby od 100 do 10 (w tej
+kolejności).
+	 */
+
+	public static void main(String[] args) {
+		for(int i=100;i>=10;i--){
+			System.out.print(i+",");
+		}
+		
+
+	}
+
+}
