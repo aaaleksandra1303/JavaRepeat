b1e3d4d438b21c3f0f820a9aaa2aae8261871460
aaaleksandra1303 <aleksandra1303@gmail.com>
2017-05-22 20:53:22 +0200

    add actionListener to buttons

--- StringRepeat/src/TicTacToe/MyFrame.java
+++ StringRepeat/src/TicTacToe/MyFrame.java
@@ -1,16 +1,43 @@
 package TicTacToe;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame{
-	
+	int counter =0;
 	public MyFrame(){
-		setSize(300, 300);
+		setSize(500, 500);
 		setTitle("TicTacToe");
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pack();
+		setLayout(new GridLayout(3, 3)); 
+		//create 9 buttons
+		for(int i=1;i<=9;i++){
+			JButton button = new JButton(""); 
+			add(button); // add buttons to myFrame
+			button.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					JButton button=(JButton)e.getSource(); //get source
+					if(counter%2==0){
+					System.out.println("X");
+					button.setText("X");
+					}else{
+						System.out.println("O");
+						button.setText("0");
+					}
+					button.setEnabled(false);//only one click in each button
+					counter++;
+				}
+			});
+		}
+		
+		//pack();
 		
 		
 
