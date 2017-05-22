0c878f3f0a15dffd289dd7bdd6f742420111dd60
aaaleksandra1303 <aleksandra1303@gmail.com>
2017-05-22 11:59:22 +0200

    printing double variable as a integer

---
+++ StringRepeat/src/Conditional/Example3.java
@@ -0,0 +1,25 @@
+package Conditional;
+
+
+
+public class Example3 {
+
+	/*
+	 * Zadanie3.
+	Napisz program, który odczytuje li czbę typu double i na ekran wypisuje całkowitą
+	wartość tej liczby.
+	 */
+	public static void main(String[] args) {
+		
+		
+		double number = 5.5;
+		int num1= (int)number;
+		
+		System.out.println("int value is "+ num1);
+		
+		
+		
+
+	}
+
+}
