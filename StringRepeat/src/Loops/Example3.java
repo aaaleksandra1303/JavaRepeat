6f21ed8bd55c173c25cd86c788df94fd261f71c4
aaaleksandra1303 <aleksandra1303@gmail.com>
2017-05-22 18:55:35 +0200

    sum of all number from 1 to 100 with loop for

---
+++ StringRepeat/src/Loops/Example3.java
@@ -0,0 +1,21 @@
+package Loops;
+
+public class Example3 {
+	/*
+	 * Zadanie3. Napisz program, który sumuje wszystkie liczby od 1 do 100.
+	 */
+
+	public static void main(String[] args) {
+	
+		int suma=0;
+		for(int i=1;i<=100;i++)
+		{
+			suma=suma+i;
+			System.out.print(suma+",");
+			
+		}
+		
+		
+	}
+
+}
