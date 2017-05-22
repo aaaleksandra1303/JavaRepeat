983189d6fe797aa7be00a48cd9875f0f0a007897
aaaleksandra1303 <aleksandra1303@gmail.com>
2017-05-22 18:54:27 +0200

    Write all number from 2 to 100 with loop for

---
+++ StringRepeat/src/Loops/Example1.java
@@ -0,0 +1,16 @@
+package Loops;
+
+public class Example1 {
+	/*
+	 * Zadanie1 . Napisz program, który wypisuje wszystkie liczby od 2 do 100.
+	 */
+
+	public static void main(String[] args) {
+		
+		for( int i=2;i<=100;i++){
+			System.out.print(i+",");
+		}
+
+	}
+
+}
