4aaf20b35df583cc8223e141ae34f4bed0bf06f7
aaaleksandra1303 <aleksandra1303@gmail.com>
2017-05-31 21:58:32 +0200

    add class sumSalaryAll to calculate sum of all employees

--- StringRepeat/src/Classes/Employee.java
+++ StringRepeat/src/Classes/Employee.java
@@ -1,10 +1,17 @@
 package Classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Zadanie4 .
 Stwórz Klasę Employee dziedziczącą po Person . Dodatkowo Employee powinien
 posiadać salary oraz commission.
 Oraz metodę annualSalary zwracającą pensje roczną.
+
+Zadanie8.
+Do klasy Employee zdefiniuj metodę statyczną zwracającą sumę wszystkich pensji
+pracowników.
  */
 public class Employee extends Person{
 	
@@ -40,13 +47,35 @@ public class Employee extends Person{
 		int anualSalary=(e.salary+e.salary*(e.commission/100))*12;
 		return anualSalary;
 	}
+	
+	public static int sumSalaryAll(List<Employee> employees){
+		int suma= 0;
+		for(Employee e: employees){
+			
+			suma= suma+anualSalary(e);
+			
+		}
+		return suma;
+	}
+	
+	
 
 	public static void main(String[] args) {
 		Person p = new Person("Ola","Bla",28,'F');
 		
 		Employee e = new Employee(p.getName(),p.getSurname(),p.getAge(),p.getSex(),10000,5);
+		Employee e2 = new Employee("Fred", "Simson", 45, 'M', 23455, 2);
+		Employee e3 = new Employee("Zenek", "Zak", 46, 'M', 123455, 3);
 		
 		System.out.println("anual salary for "+e.getName()+ " is "+anualSalary(e));
+		
+		List<Employee> employees = new ArrayList<Employee>();
+		
+		employees.add(e);
+		employees.add(e2);
+		employees.add(e3);
+		
+		System.out.println(" sum of all employees : "+sumSalaryAll(employees));
 		
 	}
 
